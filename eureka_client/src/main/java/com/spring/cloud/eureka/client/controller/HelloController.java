1c6c331ab50e13ea5319117435ccfdd4efcdb40e
zhoutaiwei <1325611131@qq.com>
2018-01-22 23:37:17 +0800

    添加peer1、2

---
+++ eureka_client/src/main/java/com/spring/cloud/eureka/client/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.spring.cloud.eureka.client.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+    @Autowired
+    private DiscoveryClient client;
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hello")
+    public String getJSON() {
+        //  return "{\"a\":[{\\\"name\\\":\\\"jobs\\\"}, {\\\"name\\\":\\\"bill\\\"},{\\\"product\\\":\\\"war3\\\"}]}";
+         ServiceInstance instance=client.getLocalServiceInstance();//过时
+        logger.info("hello, host : " + instance.getHost()+" service_id : "+instance.getServiceId());
+        return "Hello World";
+    }
+}
---
+++ eureka_client/src/test/java/com/client/test/MockmvcTest.java
@@ -0,0 +1,39 @@
+package com.client.test;
+
+import com.spring.cloud.eureka.client.Application;
+import com.spring.cloud.eureka.client.controller.HelloController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.hamcrest.Matchers.*;
+import java.util.regex.Matcher;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,classes = Application.class)
+public class MockmvcTest {
+        private MockMvc mockMvc;
+        @Before
+        public void setup() {
+            this.mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();//获取mockMvc实例
+        }
+
+        @Test
+        public void simple() throws Exception {
+            mockMvc.perform(post("/hello").accept(MediaType.APPLICATION_JSON))
+                    .andExpect(jsonPath("$.a.length()",greaterThan(2)));
+          //  .andDo(print());
+
+        }
+}//{"a":"[{"b":"hello"},{"c":"hi"},{"d":"think"}]"}

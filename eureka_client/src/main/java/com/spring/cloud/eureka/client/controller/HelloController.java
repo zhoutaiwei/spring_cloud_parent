a365eead34d4a98c49ad8b425900e909070c2a06
zhoutaiwei <1325611131@qq.com>
2018-03-05 00:03:12 +0800

    新增hystrix监控项目

--- eureka_client/src/main/java/com/spring/cloud/eureka/client/controller/HelloController.java
+++ eureka_client/src/main/java/com/spring/cloud/eureka/client/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.spring.cloud.eureka.client.controller;
 
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,8 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 
 @RestController
@@ -24,6 +25,13 @@ public class HelloController {
     public String getJSON() {
         //  return "{\"a\":[{\\\"name\\\":\\\"jobs\\\"}, {\\\"name\\\":\\\"bill\\\"},{\\\"product\\\":\\\"war3\\\"}]}"
          ServiceInstance instance=client.getLocalServiceInstance();
+        int time=new Random().nextInt(3000);
+        logger.info("时间:"+time);
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         logger.info("hello, host : " + instance.getHost()+" service_id : "+instance.getServiceId());
         return "Hello World";
     }

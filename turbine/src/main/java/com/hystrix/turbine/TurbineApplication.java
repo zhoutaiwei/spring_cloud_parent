191ed7ed452c28b71038dd79a048750f9479db14
zhoutaiwei <1325611131@qq.com>
2018-03-09 00:09:07 +0800

    添加burtine启动类

---
+++ turbine/src/main/java/com/hystrix/turbine/TurbineApplication.java
@@ -0,0 +1,10 @@
+package com.hystrix.turbine;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.amqp.EnableTurbineAmqp;
+
+@EnableTurbineAmqp
+@SpringBootApplication
+
+public class TurbineApplication {
+}

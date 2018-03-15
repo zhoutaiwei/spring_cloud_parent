7f721cd21bc41ed5e33e3ca48f10bc23c8f90099
zhoutaiwei <1325611131@qq.com>
2018-03-15 22:18:15 +0800

    周四新加

---
+++ feign_consumer/src/main/java/com/feign/consumer/fallback/HelloSericeFallback.java
@@ -0,0 +1,28 @@
+package com.feign.consumer.fallback;
+
+import com.feign.consumer.POJO.User;
+import com.feign.consumer.service.HelloService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 降级处理类，在HelloService的@FeignClient注解中指定 生效
+ */
+@Component
+public class HelloSericeFallback implements HelloService {
+    @Override
+    public String getFeignResult(@RequestParam(value = "name") String  name){
+        return "error:fallback";
+    }
+
+    @Override
+    public String getUserFeature(@RequestBody User user) {
+        return "error:fallback";
+    }
+
+    @Override
+    public User getUser(@RequestBody  User user){
+        return new User("null",0,"null");
+    }
+}

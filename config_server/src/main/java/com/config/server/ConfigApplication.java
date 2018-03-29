516d7e67c0538e87e6b362c8cca48ff448ce228a
zhoutaiwei <1325611131@qq.com>
2018-03-29 20:33:48 +0800

    周4---

--- config_server/src/main/java/com/config/server/ConfigApplication.java
+++ config_server/src/main/java/com/config/server/ConfigApplication.java
@@ -2,12 +2,13 @@ package com.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
-    public static void main(String[] args){
-        SpringApplication.run(ConfigApplication.class,args);
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigApplication.class).web(true).run(args);
     }
 }

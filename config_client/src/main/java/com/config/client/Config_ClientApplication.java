d1a68095a266fe4292d57090afd036d410cc3998
zhoutaiwei <1325611131@qq.com>
2018-04-01 23:59:47 +0800

    星期天

--- config_client/src/main/java/com/config/client/Config_ClientApplication.java
+++ config_client/src/main/java/com/config/client/Config_ClientApplication.java
@@ -2,7 +2,9 @@ package com.config.client;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient //用来发现config-server
 @SpringBootApplication
 public class Config_ClientApplication {
     public static void main(String[] args) {
